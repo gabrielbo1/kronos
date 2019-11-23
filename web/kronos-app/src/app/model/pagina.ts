da2f7ae2957ecff71ecff8f3d10b6d9376b006a9
gabrielbo1 <barbosa.olivera1@gmail.com>
2019-11-23 03:23:42 -0300

    Adicionando retornos de erro repoisotiro e busca paginada de atendimentos :construction: :construction:

---
+++ web/kronos-app/src/app/model/pagina.ts
@@ -0,0 +1,7 @@
+export class Pagina<T> {
+    public numPagina: Number;
+    public qtdPagina: Number;
+    public totalRegistro: Number;
+    public totalPagina: Number;
+    public conteudo: Array<T>;
+}
